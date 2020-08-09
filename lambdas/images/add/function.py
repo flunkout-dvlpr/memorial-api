49f2369db6add0aca69922c3afbd18f8ec4fab15
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-09 09:11:35 -0500

    Remove uncessecary code

--- lambdas/images/add/function.py
+++ lambdas/images/add/function.py
@@ -10,9 +10,7 @@ def handler(event, context):
   s3_bucket = "memorial-images"
   s3_filename = str(rawData['fileName']) + ".jepg"
 
-  s3_resource = boto3.resource('s3')
   s3 = boto3.client('s3', aws_access_key_id="AKIAZZKD3QOWJHEVVCZU", aws_secret_access_key="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")
-  bucket = s3_resource.Bucket(s3_bucket)
   try:
     uri = s3.generate_presigned_url('put_object', Params = {'Bucket': s3_bucket, 'Key': s3_filename}, ExpiresIn = 6000, HttpMethod="PUT")
     print(uri)
